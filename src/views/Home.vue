558152524b9255837db808fed3f6dcf494b6cdba
Oka Sutarto Putra <okasputra@gmail.com>
2022-06-23 11:01:39 +0700

    deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,8 @@ export default {
 
 <template>
   <section class="h-screen w-screen relative -top-20">
-    <video playsinline="" muted="muted"
-      src="../../video/y2mate.com - New Hero  Natan Animated Trailer  Mobile Legends Bang Bang_1080p.mp4" loop="loop"
-      autoplay="autoplay" class="bg-cover"></video>
+    <video playsinline="" muted="muted" src="../../video/video.mp4" loop="loop" autoplay="autoplay"
+      class="bg-cover"></video>
     <div class="pt-20 px-10 text-center relative" style="    top: -700px;">
       <h2 class="text-white font-semibold text-6xl">
         Join The Battle On <br>
